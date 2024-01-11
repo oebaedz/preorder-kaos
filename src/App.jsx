4d1c97f79e7e8c473b431ef6599d4c26d7cc78fa
oebaedz <oebaedz@gmail.com>
2024-01-11 23:30:22 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    <div className="px-2 md:px-28 bg-teal-100 h-screen">
+    </div>
+    </>
+  )
+}
+
+export default App
