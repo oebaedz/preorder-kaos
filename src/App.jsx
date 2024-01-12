7381b75b741880afb17a8321c72c54d66301b162
oebaedz <oebaedz@gmail.com>
2024-01-12 09:23:40 +0700

    added add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Contain from "./components/Contain";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
